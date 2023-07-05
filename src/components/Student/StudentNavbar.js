e4fd0ce07b7afcef331e91c620a75bf2e8b351bd
sriram-sahu <sriramsahu510@gamail.com>
2023-07-05 17:18:13 +0530

    completed

--- src/components/Student/StudentNavbar.js
+++ src/components/Student/StudentNavbar.js
@@ -3,8 +3,10 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
 const StudentNavbar = (props) => {
+  const navigate = useNavigate();
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
